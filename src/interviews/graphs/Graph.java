7f880205b21c09fd04e012ede9b0469265014360
Abhiram Kaushik <abhiramk@flipkart.com>
2014-05-12 19:11:41 +0530

    file commit dump

---
+++ src/interviews/graphs/Graph.java
@@ -0,0 +1,41 @@
+package interviews.graphs;
+
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by abhiramk on 15/03/14.
+ */
+public class Graph {
+
+    Graph (int v) {
+
+    }
+
+    Graph(Scanner sin) {
+
+    }
+
+    public int V() {
+        return 0;
+    }
+
+    public int E() {
+        return 0;
+    }
+
+    public void addEdge(int v, int w) {
+
+    }
+
+    public String toString() {
+        return "";
+    }
+
+    public Iterable<Integer> adj(int v) {
+        List<Integer> list = new ArrayList<Integer>();
+        return list;
+    }
+}
---
+++ src/javaTest/Test.java
@@ -0,0 +1,17 @@
+package javaTest;
+
+/**
+ * User: abhiramk
+ * Date: 14/11/13
+ * Time: 3:24 PM
+ * This file is the one this project deserves, but not the one it needs
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        String a = "PICKUP_Cancel";
+        String b = "Pickup_Cancelled:";
+
+        System.out.println(b.contains(a));
+    }
+}
